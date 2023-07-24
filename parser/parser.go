6d338667513552a1b307dce1da7c0e8f0d3647a4
Phill <fotofill.holland@googlemail.com>
2023-07-24 19:28:28 +0100

    implemented parser::parse

--- parser/parser.go
+++ parser/parser.go
@@ -25,5 +25,68 @@ func Parse(data string) Instruction {
 
 	stack = append(stack, &result)
 
+	var p rune
+	var current string
+	var quote_state int = 0
+	var line_state int = 0
+
+	for _, ch := range data {
+		if ch == 13 {
+		} else if (ch == ' ') && ((p == ' ') || (p == 10)) && (quote_state == 0) {
+		} else if ch == 10 {
+			if line_state == 0 {
+				line_state += 1
+			}
+		} else if ch == '"' {
+			if quote_state == 0 {
+				quote_state += 1
+			} else if quote_state == 1 {
+				if p != '\\' {
+					quote_state = 0
+				}
+			}
+			current = current + string(ch)
+		} else if ch == '{' {
+			if (line_state == 1) && (len(current) > 0) {
+				var child Instruction
+				child.value = current
+
+				current = ""
+
+				stack[len(stack)-1].children = append(stack[len(stack)-1].children, child)
+				stack = append(stack, &stack[len(stack)-1].children[len(stack[len(stack)-1].children)-1])
+
+				line_state = 0
+			}
+		} else if ch == '}' {
+			if len(current) > 0 {
+				var child Instruction
+				child.value = current
+
+				stack[len(stack)-1].children = append(stack[len(stack)-1].children, child)
+
+				current = ""
+			}
+
+			stack = stack[:len(stack)-1]
+
+		} else if line_state == 1 {
+			if len(current) > 0 {
+				var child Instruction
+				child.value = current
+
+				stack[len(stack)-1].children = append(stack[len(stack)-1].children, child)
+
+				current = ""
+			}
+
+			current = current + string(ch)
+			line_state = 0
+		} else {
+			current = current + string(ch)
+		}
+		p = ch
+	}
+
 	return result
 }
